ed4ce3648341bdd96f5e4899d080c7ff5656432c
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-11 17:12:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,14 @@
 def oxford_comma(array)
+  index = 0
   array1 = []
-  
-  array.join(", ")
+  string
+  loop do
+    if index < array.length -2
+      array1.push(array[index])
+      index += 1
+    end
+  end
+    array1.join(", ")
+    array1 << ", and #{array[index+1]}"
+    return array1 
 end
