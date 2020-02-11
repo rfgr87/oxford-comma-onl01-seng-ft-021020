d4b463b5d36383678576fc841d6b93bfb7214c8c
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-11 17:22:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,13 +2,15 @@ def oxford_comma(array)
   index = 0
   array1 = []
   string
-  loop do
-    if index < array.length -2
-      array1.push(array[index])
-      index += 1
-    end
-  end
-  array1.join(", ")
+  if array.length < 2
+      return array[0]
+    elsif array.length < 3  
+      return array1.join(", and ")
+    elsif array.length > 3
+      loop do
+        array1.push(array[index])
+        index += 1
+      
   array1 << ", and #{array[index+1]}"
   return array1 
 end
