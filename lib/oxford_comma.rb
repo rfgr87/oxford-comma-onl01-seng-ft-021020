3619c42af80c94e091f19be5c93b33616dd1e0d9
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-11 17:34:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  index = 0
+  array1 = []
+  if array.length < 2
+      return array[0]
+    elsif array.length == 2
+      string = array.join(" and ")
+      return string
+    elsif array.length > 2
+      while index < array.length - 1
+        array1.push(array[index])
+        index += 1
+      end
+    end
+    string = array1.join(", ")
+    string << ", and #{array[index]}"
+    string
 end
